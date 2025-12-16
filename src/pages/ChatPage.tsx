991c85758b8529d6f0c3cfe3ee89afa92474ec32
Virtue_hearts <admin@darknet.ca>
2025-12-16 10:20:55 -0500

    Show only live chat on chat page

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,17 +1,9 @@
 import ChatPanel from '../components/ChatPanel';
-import PromptSidePanel from '../components/PromptSidePanel';
-import UsageBar from '../components/UsageBar';
 
 export default function ChatPage() {
   return (
-    <div className="mx-auto flex max-w-6xl flex-col gap-4 px-6 py-6 lg:flex-row">
-      <div className="flex w-full flex-col gap-4 lg:w-2/3">
-        <UsageBar />
-        <ChatPanel />
-      </div>
-      <aside className="w-full shrink-0 lg:w-1/3">
-        <PromptSidePanel />
-      </aside>
+    <div className="mx-auto flex max-w-4xl px-6 py-6">
+      <ChatPanel />
     </div>
   );
 }
